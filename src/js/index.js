7376c488450b1f84ad591950f8ef0bc53780a482
Sayah Reda <reda1998.rs@gmail.com>
2021-12-29 15:39:56 +0100

    set the add and delete button

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import '../css/main.scss';
 __webpack_public_path__ = '/some-custom-public-path';
+
+document.getElementById('up').onclick = function() {
+  document.getElementById('qty').value++;
+};
+document.getElementById('down').onclick = function() {
+  if (document.getElementById('qty').value > 0) {
+    document.getElementById('qty').value--;
+  }
+};
